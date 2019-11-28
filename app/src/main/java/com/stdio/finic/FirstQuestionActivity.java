8bcbe7230420a8d2b1107459238ee64bfb45f03c
Std1o <kwork-stdio@mail.ru>
2019-11-28 21:31:01 +0300

    0.0.8

--- app/src/main/java/com/stdio/finic/FirstQuestionActivity.java
+++ app/src/main/java/com/stdio/finic/FirstQuestionActivity.java
@@ -3,7 +3,10 @@ package com.stdio.finic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class FirstQuestionActivity extends AppCompatActivity {
 
@@ -16,5 +19,22 @@ public class FirstQuestionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_first_question);
         progressBar = findViewById(R.id.progressId);
         progressBar.setProgress(70);
+
+        RadioButton redRadioButton = findViewById(R.id.radioButton1);
+        redRadioButton.setOnClickListener(radioButtonClickListener);
+
+        RadioButton greenRadioButton = findViewById(R.id.radioButton2);
+        greenRadioButton.setOnClickListener(radioButtonClickListener);
+
+        RadioButton blueRadioButton = findViewById(R.id.radioButton3);
+        blueRadioButton.setOnClickListener(radioButtonClickListener);
     }
+
+    View.OnClickListener radioButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            RadioButton rb = (RadioButton)v;
+            Toast.makeText(FirstQuestionActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
+        }
+    };
 }

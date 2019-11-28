31acf41ef8385164336b9028f0f40f30d7cb21d4
Std1o <kwork-stdio@mail.ru>
2019-11-28 19:03:13 +0300

    0.0.2

---
+++ app/src/main/java/com/stdio/finic/FirstQuestionActivity.java
@@ -0,0 +1,19 @@
+package com.stdio.finic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class FirstQuestionActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_question);
+        progressBar = findViewById(R.id.progressId);
+        progressBar.setProgress(50);
+    }
+}

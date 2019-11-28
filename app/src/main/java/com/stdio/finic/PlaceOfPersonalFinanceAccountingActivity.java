be764a88102dd4415ed6ddb2650b19ad809a9c3f
Std1o <kwork-stdio@mail.ru>
2019-11-28 22:08:32 +0300

    0.0.9

--- app/src/main/java/com/stdio/finic/PlaceOfPersonalFinanceAccountingActivity.java
+++ app/src/main/java/com/stdio/finic/PlaceOfPersonalFinanceAccountingActivity.java
@@ -2,23 +2,22 @@ package com.stdio.finic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
-public class FirstQuestionActivity extends AppCompatActivity {
+public class PlaceOfPersonalFinanceAccountingActivity extends AppCompatActivity {
 
     private ProgressBar progressBar;
-    int id = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme_ToolBar);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first_question);
+        setContentView(R.layout.activity_place_of_personal_finance_accounting);
+
         progressBar = findViewById(R.id.progressId);
         progressBar.setProgress(70);
 
@@ -36,14 +35,7 @@ public class FirstQuestionActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             RadioButton rb = (RadioButton)v;
-            id = rb.getId();
-            Toast.makeText(FirstQuestionActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(PlaceOfPersonalFinanceAccountingActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
         }
     };
-
-    public void onClick(View view) {
-        if (id == R.id.radioButton1 || id == R.id.radioButton2) {
-            startActivity(new Intent(FirstQuestionActivity.this, PlaceOfPersonalFinanceAccountingActivity.class));
-        }
-    }
 }

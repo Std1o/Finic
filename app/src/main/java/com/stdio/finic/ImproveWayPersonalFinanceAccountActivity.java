6a3be45517ae248bca1abb3d4980653c691667f9
Std1o <kwork-stdio@mail.ru>
2019-12-01 15:15:19 +0300

    0.1.6

--- app/src/main/java/com/stdio/finic/ImproveWayPersonalFinanceAccountActivity.java
+++ app/src/main/java/com/stdio/finic/ImproveWayPersonalFinanceAccountActivity.java
@@ -2,7 +2,9 @@ package com.stdio.finic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ImproveWayPersonalFinanceAccountActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ImproveWayPersonalFinanceAccountActivity extends AppCompatActivity 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_improve_way_personal_finance_account);
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent(this, ReasonForAccountingForPersonaFinancesActivity.class));
+
     }
 }
---
+++ app/src/main/java/com/stdio/finic/ReasonForAccountingForPersonaFinancesActivity.java
@@ -0,0 +1,14 @@
+package com.stdio.finic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReasonForAccountingForPersonaFinancesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reason_for_accounting_for_persona_finances);
+    }
+}

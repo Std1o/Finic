5699895db7eba96ef630fdc633080311432f9e45
Std1o <kwork-stdio@mail.ru>
2019-12-01 15:08:29 +0300

    0.1.5

---
+++ app/src/main/java/com/stdio/finic/ImproveWayPersonalFinanceAccountActivity.java
@@ -0,0 +1,14 @@
+package com.stdio.finic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImproveWayPersonalFinanceAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_improve_way_personal_finance_account);
+    }
+}
--- app/src/main/java/com/stdio/finic/NecessaryFunctionalActivity.java
+++ app/src/main/java/com/stdio/finic/NecessaryFunctionalActivity.java
@@ -2,7 +2,9 @@ package com.stdio.finic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NecessaryFunctionalActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class NecessaryFunctionalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_necessary_functional);
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent(this, ImproveWayPersonalFinanceAccountActivity.class));
+
     }
 }

8251b6244a4bc3f85e0108a17bdfe02f77e44d0e
Std1o <kwork-stdio@mail.ru>
2019-12-01 15:52:02 +0300

    0.1.8

--- app/src/main/java/com/stdio/finic/FinancialAccountingIncentiveActivity.java
+++ app/src/main/java/com/stdio/finic/FinancialAccountingIncentiveActivity.java
@@ -2,7 +2,9 @@ package com.stdio.finic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FinancialAccountingIncentiveActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class FinancialAccountingIncentiveActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_financial_accounting_incentive);
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent(this, NecessaryFunctionalActivityLine2.class));
+
     }
 }

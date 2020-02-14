e493b7c254ba61d1c86692ee25fa78d070083b64
Std1o <37378410+Std1o@users.noreply.github.com>
2020-02-14 20:59:05 +0300

    0.8.5

--- app/src/main/java/com/stdio/finic/slideShow/SlideActivity1.java
+++ app/src/main/java/com/stdio/finic/slideShow/SlideActivity1.java
@@ -2,16 +2,68 @@ package com.stdio.finic.slideShow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 import com.stdio.finic.R;
 
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
 public class SlideActivity1 extends AppCompatActivity {
+
+    private final CompositeDisposable disposables = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slide1);
+        doSomeWork();
+    }
+
+    private void doSomeWork() {
+        disposables.add(getObservable()
+                // Run on a background thread
+                .subscribeOn(Schedulers.io())
+                // Be notified on the main thread
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(getObserver()));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposables.clear(); // clearing it : do not emit after destroy
+    }
+
+    private Observable<? extends Long> getObservable() {
+        return Observable.interval(3, 3, TimeUnit.SECONDS);
+    }
+
+    private DisposableObserver<Long> getObserver() {
+        return new DisposableObserver<Long>() {
+
+            @Override
+            public void onNext(Long value) {
+                startActivity(new Intent(SlideActivity1.this, SlideActivity2.class));
+                Animatoo.animateFade(SlideActivity1.this);
+                finish();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+            }
+
+            @Override
+            public void onComplete() {
+            }
+        };
     }
 }

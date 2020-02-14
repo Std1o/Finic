2aab79950b006fe36a6fa2fce14d715adcc8d8e9
Std1o <37378410+Std1o@users.noreply.github.com>
2020-02-14 21:25:08 +0300

    0.8.6

---
+++ app/src/main/java/com/stdio/finic/slideShow/SlideActivity3.java
@@ -0,0 +1,17 @@
+package com.stdio.finic.slideShow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.stdio.finic.R;
+
+public class SlideActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide3);
+    }
+}
